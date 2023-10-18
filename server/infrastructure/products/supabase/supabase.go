d47d0f3a18d41772d46943f9155f0805a942682b
Lucas-Linhar3s <linhar3s77@gmail.com>
2023-10-18 15:58:25 -0300

    Created infrastructure products

---
+++ server/infrastructure/products/supabase/supabase.go
@@ -0,0 +1,48 @@
+package supabase
+
+import (
+	"github.com/Lucas-Linhar3s/TheBarber/database"
+	"github.com/Lucas-Linhar3s/TheBarber/infrastructure/products"
+	"github.com/google/uuid"
+)
+
+// PGRepository struct
+type PGRepository struct {
+	DB *database.Client
+}
+
+// GetAllProducts returns all products from the PGRepository.
+func (r *PGRepository) GetAllProducts() (products []products.Product, err error) {
+	return products, nil
+}
+
+// GetProductByID retrieves a product from the PGRepository based on its ID.
+func (r *PGRepository) GetProductByID(id uuid.UUID) (product products.Product, err error) {
+	return product, nil
+}
+
+// CreateProduct creates a new product in the PGRepository.
+func (r *PGRepository) CreateProduct(product products.Product) (productID *uuid.UUID, err error) {
+	_, count, err := r.DB.From("product").Insert(product, true, "", "*", "exact").Execute()
+	if err != nil {
+		return productID, err
+	}
+
+	if count == 0 {
+		return productID, err
+	}
+
+	productID = &product.ID
+
+	return
+}
+
+// DeleteProduct deletes a product from the PGRepository.
+func (r *PGRepository) DeleteProduct(id uuid.UUID) error {
+	return nil
+}
+
+// UpdateProduct updates the specified product in the PGRepository.
+func (r *PGRepository) UpdateProduct(product products.Product) error {
+	return nil
+}
